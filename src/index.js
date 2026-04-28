eff069d16d08953abe77669de400b01c300e162c
jhlee111 <jhlee111@gmail.com>
2026-04-27 23:04:16 -0700

    build(gs-num-pad): wire into multi-entry build
    Adds gs-num-pad to src/index.js re-exports, vite.config.js entry, and
    package.json exports/keywords.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { GsBirthdayPicker } from './gs-birthday-picker/gs-birthday-picker.js';
+export { GsNumPad } from './gs-num-pad/gs-num-pad.js';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
+import tidewave from 'tidewave/vite-plugin';
 
 export default defineConfig({
+  plugins: [tidewave()],
   build: {
     lib: {
       entry: {
         'gs-webcomponents': 'src/index.js',
         'gs-birthday-picker': 'src/gs-birthday-picker/gs-birthday-picker.js',
+        'gs-num-pad': 'src/gs-num-pad/gs-num-pad.js',
       },
       formats: ['es'],
     },
